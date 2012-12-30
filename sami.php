026eb5b8af78d6ed2dab48f2c584d5160a671282
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2012-12-30 16:01:19 -0600

    using sami to generate API docs

---
+++ sami.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file allows documentation to be generated based on the docblocks
+ * in the files.
+ *
+ * @see https://github.com/fabpot/Sami
+ */
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in(__DIR__ . '/src');
+
+return new Sami($iterator, array(
+    'title' => 'nbobtc/bitcoind-php API Docs',
+    'build_dir' => __DIR__ . '/docs',
+));
