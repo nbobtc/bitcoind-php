61bb5bce9ff826b66c79bedb5c5b4901b131d326
Joshua Estes <Joshua.Estes@iostudio.com>
2013-01-02 15:07:19 -0600

    updated phpunit tests and the xml dist for phpunit

--- Tests/Nbobtc/Bitcoind/BitcoindTest.php
+++ Tests/Nbobtc/Bitcoind/BitcoindTest.php
@@ -1,11 +1,296 @@
 <?php
 
-use Nbobtc\Bitcoind\Bitcoind;
-
 class BitcoindTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testGetInfo()
+    public function testAddmultisigaddress()
     {
+        $this->markTestIncomplete();
     }
+
+    public function testBackupwallet()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCreaterawtransaction()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testDecoderawtransaction()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testDumpprivkey()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testEncrypteallet()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetaccount()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetaccountaddress()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetbalance()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetblock()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetblockcount()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetblockhash()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetblocktemplate()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetconnectioncount()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetdifficulty()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetgenerate()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGethashespersec()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetinfo()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetmininginfo()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetnewaddress()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetpeerinfo()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetrawmempool()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetrawtransaction()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetreceivedbyaccount()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetreceivedbyaddress()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGettransaction()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGettxout()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGettxoutsetinfo()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetwork()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testHelp()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testImpoerprivkey()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testKeypoolrefill()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testListaccounts()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testListaddressbygroupings()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testListlockunspent()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testListreceivedbyaccount()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testListreceivedbyaddress()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testListsinceblock()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testListtransactions()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testListunspent()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testLockunspent()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testMove()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testSendfrom()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testSendmany()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testSendrawtransaction()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testSendtoaddress()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testSetaccount()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testSetgenerate()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testSettxfee()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testSignmessage()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testSignrawtransaction()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testStop()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testSubmitblock()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testValidateaddress()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testVerifymessage()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testWalletlock()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testWalletpassphrase()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testWalletpassphrasechange()
+    {
+        $this->markTestIncomplete();
+    }
+
 }
